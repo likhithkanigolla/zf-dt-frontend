738528465fe73b5c88086a0539b01f94c9fc3572
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-03-08 15:13:33 +0530

    Added Grafana page

--- src/pages/VisualizationPage/VisualizationPage.js
+++ src/pages/VisualizationPage/VisualizationPage.js
@@ -7,9 +7,8 @@ const VisualizationPage = () => {
   return (
     <div className="visualization-page">
     <NavigationBar />
-      <h2>Visualization Page</h2>
-      <p>This is the Visualization page.</p>
-    </div>
+    <iframe src="http://10.3.1.117:3000/d/c9998c83-4255-4c0d-ad26-524b8b84272d/zf-digitaltwin?orgId=1&kiosk&refresh=5s&autofitpanels" width="100%" height="800px" frameborder="0"></iframe>
+   </div>
   );
 }
 
