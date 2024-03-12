925dd75392066e75d18b76c69f068e925634f569
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-03-13 01:29:54 +0530

    Removed refresh rate from grafana url

--- src/pages/VisualizationPage/VisualizationPage.js
+++ src/pages/VisualizationPage/VisualizationPage.js
@@ -7,7 +7,7 @@ const VisualizationPage = () => {
   return (
     <div className="visualization-page">
     <NavigationBar />
-    <iframe src="http://10.3.1.117:3000/d/c9998c83-4255-4c0d-ad26-524b8b84272d/zf-digitaltwin?orgId=1&kiosk&refresh=5s&autofitpanels" width="100%" height="800px" frameborder="0"></iframe>
+    <iframe src="http://10.3.1.117:3000/d/c9998c83-4255-4c0d-ad26-524b8b84272d/zf-digitaltwin?orgId=1&kiosk&autofitpanels" width="100%" height="800px" frameborder="0"></iframe>
    </div>
   );
 }
