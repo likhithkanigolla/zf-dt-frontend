e14144bbf8ba41a38700047a14c0f0d4c0ff825f
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-03-31 23:35:40 +0530

    Updated Visualization URL

--- src/pages/VisualizationPage/VisualizationPage.js
+++ src/pages/VisualizationPage/VisualizationPage.js
@@ -7,7 +7,7 @@ const VisualizationPage = () => {
   return (
     <div className="visualization-page">
      <NavigationBar title="Digital Twin for Water Quality - Visualization"/>
-    <iframe src="http://10.3.1.117:3000/d/c9998c83-4255-4c0d-ad26-524b8b84272d/zf-digitaltwin?orgId=1&kiosk&autofitpanels" width="100%" height="800px" frameborder="0"></iframe>
+    <iframe src="https://smartcitylivinglab.iiit.ac.in/grafana/d/c9998c83-4255-4c0d-ad26-524b8b84272d/zf-digital-twin?orgId=1&kiosk&autofitpanels" width="100%" height="800px" frameborder="0"></iframe>
    </div>
   );
 }
