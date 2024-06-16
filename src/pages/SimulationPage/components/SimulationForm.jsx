0be8ae0ece9476ddda6eef47b4185a1e4dc96e71
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-06-16 16:58:46 +0530

    Updated Form added new feilds

--- src/pages/SimulationPage/components/SimulationForm.jsx
+++ src/pages/SimulationPage/components/SimulationForm.jsx
@@ -1,73 +1,181 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './SimulationForm.css';
 
 function SimulationForm({ SoilQuantity, setSoilQuantity, SandQuantity, setSandQuantity, inputValues, handleChange, handleStartSimulation, isSimulationRunning }) {
+    const [isWaterConfigCollapsed, setIsWaterConfigCollapsed] = useState(false);
+    const [isCapacitiesCollapsed, setIsCapacitiesCollapsed] = useState(false);
+    const [isROPlantConfigCollapsed, setIsROPlantConfigCollapsed] = useState(false);
+    const [isMotorConfigCollapsed, setIsMotorConfigCollapsed] = useState(false);
+
+    const toggleWaterConfig = () => {
+        setIsWaterConfigCollapsed(!isWaterConfigCollapsed);
+    };
+
+    const toggleCapacities = () => {
+        setIsCapacitiesCollapsed(!isCapacitiesCollapsed);
+    };
+
+    const toggleROPlantConfig = () => {
+        setIsROPlantConfigCollapsed(!isROPlantConfigCollapsed);
+    };
+
+    const toggleMotorConfig = () => {
+        setIsMotorConfigCollapsed(!isMotorConfigCollapsed);
+    };
+
     return (
-        <div className="container" style={{ flex: 1 }}>
-            <h4 className="heading" htmlFor="SoilQuantity">
-                Soil Impurities(In grams){" "}
-            </h4>
-            <input
-                type="number"
-                name="SoilQuantity"
-                id="SoilQuantity"
-                value={SoilQuantity}
-                onChange={(e) => {
-                    setSoilQuantity(e.target.value);
-                    handleChange(e);
-                }}
-            />
-            <h4 className="heading" htmlFor="SandQuantity">
-                Sand Impurities(In grams){" "}
-            </h4>
-            <input
-                type="number"
-                name="SandQuantity"
-                id="SandQuantity"
-                value={SandQuantity}
-                onChange={(e) => {
-                    setSandQuantity(e.target.value);
-                    handleChange(e);
-                }}
-            />
-            <h4 className="heading">Temperature(°C):</h4>
-            <input
-                className="input-box"
-                type="number"
-                name="temperature"
-                id="temperature"
-                value={inputValues.temperature}
-                onChange={handleChange}
-            />
-            <br />
-            <h4 className="heading">Desired TDS(mg/Litre):</h4>
-            <input
-                className="input-box"
-                type="number"
-                name="desired_tds"
-                id="desired_tds"
-                value={inputValues.desired_tds}
-                onChange={handleChange}
-            />
-            <h4 className="heading">Effective Membrane Area(m²):</h4>
-            <input
-                className="input-box"
-                type="number"
-                name="effective_membrane_area"
-                id="effective_membrane_area"
-                value={inputValues.effective_membrane_area}
-                onChange={handleChange}
-            />
-            <br />
-            <h4 className="heading">Sump Capacity (Liters):</h4>
-            <input
-                className="input-box"
-                type="number"
-                name="sumpCapacity"
-                id="sumpCapacity"
-                value={inputValues.sumpCapacity}
-                onChange={handleChange}
-            />
-            <button onClick={handleStartSimulation} className="button">
+        <div>
+        <div className="container" style={{ flex: 1, overflowY: 'scroll', height: '75vh', color: 'white' }}>
+            <h3 className="heading" onClick={toggleWaterConfig}>
+                Water Configuration
+            </h3>
+            {isWaterConfigCollapsed && (
+                <>
+                    <h4 className="heading-in" htmlFor="SoilQuantity">
+                        Soil Impurities (In grams)
+                    </h4>
+                    <input
+                        type="number"
+                        name="SoilQuantity"
+                        id="SoilQuantity"
+                        value={SoilQuantity}
+                        className='input-box'
+                        onChange={(e) => {
+                            setSoilQuantity(e.target.value);
+                            handleChange(e);
+                        }}
+                    />
+                    <h4 className="heading-in" htmlFor="SandQuantity">
+                        Sand Impurities (In grams)
+                    </h4>
+                    <input
+                        type="number"
+                        name="SandQuantity"
+                        id="SandQuantity"
+                        value={SandQuantity}
+                        onChange={(e) => {
+                            setSandQuantity(e.target.value);
+                            handleChange(e);
+                        }}
+                    />
+                    <h4 className="heading-in">Temperature (°C):</h4>
+                    <input
+                        className="input-box"
+                        type="number"
+                        name="temperature"
+                        id="temperature"
+                        value={inputValues.temperature}
+                        onChange={handleChange}
+                    />
+                </>
+            )}
+
+            <h3 className="heading" onClick={toggleCapacities}>
+                Capacities
+            </h3>
+            {isCapacitiesCollapsed && (
+                <>
+                    <h4 className="heading-in">Sump Capacity (Liters):</h4>
+                    <input
+                        className="input-box"
+                        type="number"
+                        name="sumpCapacity"
+                        id="sumpCapacity"
+                        value={inputValues.sumpCapacity}
+                        onChange={handleChange}
+                    />
+                    <h4 className="heading-in">OHT Capacity (Liters):</h4>
+                    <input
+                        className="input-box"
+                        type="number"
+                        name="ohtCapacity"
+                        id="ohtCapacity"
+                        value={inputValues.ohtCapacity}
+                        onChange={handleChange}
+                    />
+                </>
+            )}
+
+            <h3 className="heading" onClick={toggleROPlantConfig}>
+                RO Plant Configuration
+            </h3>
+            {isROPlantConfigCollapsed && (
+                <>
+                    <h4 className="heading-in">Desired TDS (After Filtration):</h4>
+                    <input
+                        className="input-box"
+                        type="number"
+                        name="desired_tds"
+                        id="desired_tds"
+                        value={inputValues.desired_tds}
+                        onChange={handleChange}
+                    />
+                    <h4 className="heading-in">Membrane Area (m²):</h4>
+                    <input
+                        className="input-box"
+                        type="number"
+                        name="membrane_area"
+                        id="membrane_area"
+                        value={inputValues.membrane_area}
+                        onChange={handleChange}
+                    />
+                    <h4 className="heading-in">RO OHT Capacity (Liters):</h4>
+                    <input
+                        className="input-box"
+                        type="number"
+                        name="ro_ohtCapacity"
+                        id="ro_ohtCapacity"
+                        value={inputValues.ro_ohtCapacity}
+                        onChange={handleChange}
+                    />
+                </>
+            )}
+
+            <h3 className="heading" onClick={toggleMotorConfig}>
+                Motor Configuration
+            </h3>
+            {isMotorConfigCollapsed && (
+                <>
+                    <h4 className="heading-in">Voltage:</h4>
+                    <input
+                        className="input-box"
+                        type="number"
+                        name="voltage"
+                        id="voltage"
+                        value={inputValues.voltage}
+                        onChange={handleChange}
+                    />
+                    <h4 className="heading-in">Current:</h4>
+                    <input
+                        className="input-box"
+                        type="number"
+                        name="current"
+                        id="current"
+                        value={inputValues.current}
+                        onChange={handleChange}
+                    />
+                    <h4 className="heading-in">Power Factor:</h4>
+                    <input
+                        className="input-box"
+                        type="number"
+                        name="power_factor"
+                        id="power_factor"
+                        value={inputValues.power_factor}
+                        onChange={handleChange}
+                    />
+                    <h4 className="heading-in">Motor Efficiency:</h4>
+                    <input
+                        className="input-box"
+                        type="number"
+                        name="motor_efficiency"
+                        id="motor_efficiency"
+                        value={inputValues.motor_efficiency}
+                        onChange={handleChange}
+                    />
+                </>
+            )}
+        </div>
+        <button onClick={handleStartSimulation} className="button">
                 {isSimulationRunning ? "Stop Simulation" : "Start Simulation"}
             </button>
         </div>
