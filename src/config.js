e1377d23fab2ff23e9724462c2351bd07c33d488
Bala Likhith Kanigolla <55309319+likhithkanigolla@users.noreply.github.com>
2024-06-30 14:59:42 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 // config.js
 const config = {
-    backendAPI : "http://smartcitylivinglab.iiit.ac.in:1629", 
+    backendAPI : "https://smartcitylivinglab.iiit.ac.in/zf-backend-api", 
+    middlewareAPI: "http://smartcitylivinglab.iiit.ac.in:1630"
     // backendAPI: "http://localhost:1629",
-    middlewareAPI: "http://localhost:1630"
+    // middlewareAPI: "http://localhost:1630"
 };
 
 export default config;
