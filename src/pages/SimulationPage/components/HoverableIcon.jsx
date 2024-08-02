ddca15078415c595affa64ea8336508831291264
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-08-02 13:12:24 +0530

    Changes for Joyride

--- src/pages/SimulationPage/components/HoverableIcon.jsx
+++ src/pages/SimulationPage/components/HoverableIcon.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const HoverableIcon = ({ src, alt, onClick, dataId, data, rotation, refreshData }) => {
+const HoverableIcon = ({ src, alt, onClick, dataId, data, rotation, refreshData, waterLevelNodeWorking, setWaterLevelNodeWorking, type}) => {
   const [isHovered, setIsHovered] = useState(false);
   const [isEnabled, setIsEnabled] = useState(true);
 
@@ -10,6 +10,7 @@ const HoverableIcon = ({ src, alt, onClick, dataId, data, rotation, refreshData 
     }
     else{
     setIsEnabled(!isEnabled);
+    setWaterLevelNodeWorking(!waterLevelNodeWorking);
     }
   };
 
@@ -30,8 +31,10 @@ const HoverableIcon = ({ src, alt, onClick, dataId, data, rotation, refreshData 
           width: '2vw',
           height: '2vw',
           borderRadius: '50%', // Makes the image circular (adjust as needed)
-          zIndex:15
-        }}
+          zIndex:15,
+          }}
+          className= {type === 'waterquantitysensor' ? 'waterquantity-vtool' : type === 'waterlevelsensor' ? 'waterlevel-vtool' : type === 'motorsensor' ? 'motor-vtool' : type === 'leakagesensor' ? 'leakage-vtool' : type === 'waterqualitysensor' ? 'waterquality-vtool': 'normal-vtool'}
+        
         onClick={onClick ? onClick : handleClick} 
       />
       {isEnabled && isHovered && (
